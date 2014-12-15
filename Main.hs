b1565a2600c86722491d5f1460a3ac9160a2e188
Vlatko Klabucar <vlatko.klabucar@gmail.com>
2014-12-15 19:26:18 +0100

    Added split depencendy, implementted file parsing

--- Main.hs
+++ Main.hs
@@ -1,7 +1,9 @@
 import Solve
 import System.Environment
+import Data.List.Split
 
 main = do
   args <- getArgs
   f <- readFile $ args!!0
-  putStr f
+  let spl = map ( map (splitOn "\t")) . map (splitOn "\r\n") . splitOn "\r\n\r\n" $ f
+  putStrLn . show $ spl
