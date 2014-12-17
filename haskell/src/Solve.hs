28db7bffbf9322489681916cadca255f0fa27c09
Vlatko <yossarin.github@gmail.com>
2014-12-17 12:28:52 +0100

    Added distance function.

--- haskell/src/Solve.hs
+++ haskell/src/Solve.hs
@@ -19,5 +19,8 @@ type Customer  = (Vertex, Demand)
 type Warehouse = (Vertex, Capacity, Cost)
 type Graph     = ([Warehouse], [Customer], TruckCap, TruckCost) 
 
+distance :: Vertex -> Vertex -> Int
+distance (x1, y1) (x2, y2) = truncate . (*100) . sqrt . fromIntegral $ (x1-x2)^2 + (y1-y2)^2
+
 solve :: Graph -> IO ()
 solve _ = putStrLn "Bazzzzzinga!"
