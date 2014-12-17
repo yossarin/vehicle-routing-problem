34d203399de7ffffd86bfe4713bb5e75e2a83a07
Teon Banek <theongugl@gmail.com>
2014-12-17 22:12:54 +0100

    Renamed distance function to travelCost and extracted euclideanDistance function.

--- haskell/src/Solve.hs
+++ haskell/src/Solve.hs
@@ -19,8 +19,12 @@ type Customer  = (Vertex, Demand)
 type Warehouse = (Vertex, Capacity, Cost)
 type Graph     = ([Warehouse], [Customer], TruckCap, TruckCost) 
 
-distance :: Vertex -> Vertex -> Int
-distance (x1, y1) (x2, y2) = truncate . (*100) . sqrt . fromIntegral $ (x1-x2)^2 + (y1-y2)^2
+euclideanDistance :: Vertex -> Vertex -> Float
+euclideanDistance (x1, y1) (x2, y2) =
+  sqrt . fromIntegral $ (x1 - x2)^2 + (y1 - y2)^2
+
+travelCost :: Vertex -> Vertex -> Cost
+travelCost a = truncate . (*100) . euclideanDistance a
 
 solve :: Graph -> IO ()
 solve _ = putStrLn "Bazzzzzinga!"
