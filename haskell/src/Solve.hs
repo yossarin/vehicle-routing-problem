dd3a8612c9c02c4405c854255241b6f06fe36719
Vlatko Klabucar <vlatko.klabucar@gmail.com>
2015-01-04 07:03:38 +0100

    function pairwisePermutation added.

--- haskell/src/Solve.hs
+++ haskell/src/Solve.hs
@@ -26,5 +26,13 @@ euclideanDistance (x1, y1) (x2, y2) =
 travelCost :: Vertex -> Vertex -> Cost
 travelCost a = truncate . (*100) . euclideanDistance a
 
+pairwisePermutation :: [a] -> [[a]]
+pairwisePermutation []       = []
+pairwisePermutation (x:[])   = [[x]]
+pairwisePermutation (x:y:[]) = [[x,y],[y,x]]
+pairwisePermutation xs       = pwP xs (length xs)
+  where pwP ys 1 = [[last ys] ++ (tail . init $ ys) ++ [head ys], ys]
+        pwP ys n = ((take (n-2) ys) ++ [ys!!(n-1)] ++ [ys!!(n-2)] ++ (drop n ys)):(pwP ys (n-1))
+
 solve :: Graph -> IO ()
 solve _ = putStrLn "Bazzzzzinga!"
