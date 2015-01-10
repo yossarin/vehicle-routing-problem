0f0763c2c2525ab7ef5d728e77b6e436c5f36b2b
vlatko <vlatko.klabucar@gmail.com>
2015-01-10 02:51:53 +0100

    run.sh: bug fix

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#  This script looks for all the files that start with params*,
-#  and runs the hmo-project executable using the params* files
+#  This script looks for all the files that start with params*.txt,
+#  and runs the hmo-project executable using the params*.txt files
 #  as parameters for each run. The results are saved in the same
 #  directory as $FILENAME.res
 
-for P in `ls params*`
+for P in `ls params*.txt`
 do
     echo " Runnig with parameters $P"
     ./haskell/dist/build/hmo-project/hmo-project in.txt $P +RTS -N4 > $P.res
